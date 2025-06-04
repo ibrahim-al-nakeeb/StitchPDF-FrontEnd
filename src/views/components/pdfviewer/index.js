ef236a78db76b45198ce703cf63c09ff9ce2d1c6
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-04 17:12:21 +0300

    Support Blob input in PdfViewer using object URLs

--- src/views/components/pdfviewer/index.js
+++ src/views/components/pdfviewer/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Box} from "@mui/material";
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
@@ -8,14 +8,24 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 
 export default function PdfViewer({ document }) {
-
     const layoutPlugin = defaultLayoutPlugin();
+    const [objectUrl, setObjectUrl] = useState(null);
+
+    useEffect(() => {
+        if (!document) return;
+        const url = typeof document === 'string' ? document : URL.createObjectURL(document);
+        setObjectUrl(url);
+
+        return () => URL.revokeObjectURL(url);
+    }, [document]);
+
+    if (!objectUrl) return null;
 
     return(
         <Box sx={{ width: "100%", height: "100%" }}>
             <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100%", height: "inherit"}}>
                 <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                    <Viewer fileUrl={document} plugins={[layoutPlugin]}/>
+                    <Viewer fileUrl={objectUrl} plugins={[layoutPlugin]}/>
                 </Worker>
             </Box>
         </Box>
