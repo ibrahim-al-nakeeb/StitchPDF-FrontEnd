9fb9a8a9db07774a256a435302ddcc593e608ff7
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-04 17:23:38 +0300

    Export API hooks from central index file

---
+++ src/hooks/index.js
@@ -0,0 +1,9 @@
+import { useGenerateGroupID } from "./useGenerateGroupID";
+import { useDownloadMergedPdf } from "./useDownloadMergedPdf";
+import { useUploadFile } from "./useUploadFile";
+
+export {
+    useGenerateGroupID,
+    useDownloadMergedPdf,
+    useUploadFile
+};
