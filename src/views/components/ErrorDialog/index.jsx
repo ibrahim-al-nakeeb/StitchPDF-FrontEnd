c003e1094bd98ad8549667879e48242c27098172
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-07 13:48:04 +0300

    Add ErrorDialog component

---
+++ src/views/components/ErrorDialog/index.jsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    Button
+} from '@mui/material';
+
+const ErrorDialog = ({ open, onClose, title, message }) => {
+    return (
+        <Dialog
+            open={open}
+            onClose={onClose}
+            aria-labelledby="draggable-dialog-title"
+        >
+            <DialogTitle
+                style={{ cursor: 'move', color: 'red' }}
+                id="draggable-dialog-title"
+            >
+                {title}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText>{message}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose}>Close</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default ErrorDialog;
