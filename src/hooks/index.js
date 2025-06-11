f7209a9d2a5ffac0320cdfe95ede1fbe6452d954
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-11 23:58:08 +0300

    Switch to default exports for API hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,6 @@
-import { useGenerateGroupID } from "./useGenerateGroupID";
-import { useDownloadMergedPdf } from "./useDownloadMergedPdf";
-import { useUploadFile } from "./useUploadFile";
+import useGenerateGroupID from "./useGenerateGroupID";
+import useDownloadMergedPdf from "./useDownloadMergedPdf";
+import useUploadFile from "./useUploadFile";
 
 export {
     useGenerateGroupID,
