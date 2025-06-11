c5bbe921f90198d01768aeb54d103ede2d56c919
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-11 23:58:58 +0300

    Rename session_tag key to groupId in metadata JSON

--- src/views/Main/helpers.js
+++ src/views/Main/helpers.js
@@ -1,6 +1,6 @@
 export const createSessionMetadataFile = (groupId) => {
     const jsonBlob = new Blob(
-        [JSON.stringify({ session_tag: groupId }, null, 2)],
+        [JSON.stringify({ groupId: groupId }, null, 2)],
         { type: 'application/json' }
     );
     const file = new File([jsonBlob], `${groupId}.json`, {
