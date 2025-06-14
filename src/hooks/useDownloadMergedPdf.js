4fd63f3ee02b4cc132f853053029cc7bad63106a
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-14 22:24:55 +0300

    Add error handling to useDownloadMergedPdf hook

--- src/hooks/useDownloadMergedPdf.js
+++ src/hooks/useDownloadMergedPdf.js
@@ -2,22 +2,39 @@ import axios from 'axios';
 
 const useDownloadMergedPdf = () => {
     const downloadMergedPdf = async (groupId) => {
-        const response = await axios.get(
-            `${process.env.REACT_APP_API_URL}/download-url?groupId=${groupId}`, {
-                headers: { 
-                    'X-Api-Key': process.env.REACT_APP_API_KEY
-                }
+        try {
+            if (!groupId) {
+                console.error("Missing  or groupId");
+                throw new Error("Missing required parameters");
             }
-        );
-        
-        const { presigned_url } = await response.data;
 
-        const link = document.createElement('a');
-        link.href = presigned_url;
-        link.target = '_blank';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+            const response = await axios.get(
+                `${process.env.REACT_APP_API_URL}/download-url?groupId=${groupId}`, {
+                    headers: { 
+                        'X-Api-Key': process.env.REACT_APP_API_KEY
+                    }
+                }
+            );
+
+            const { presigned_url } = await response.data;
+
+            const link = document.createElement('a');
+            link.href = presigned_url;
+            link.target = '_blank';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+        } catch (error) {            
+            const message = error.response?.data?.errorMessage || error.message || 'Unknown error';
+            const status = error.response?.status;
+
+            const customError = new Error(message);
+            customError.status = status;
+
+            console.error("Error in uploadFile:", message);
+            throw customError;
+        }
     };
 
     return { downloadMergedPdf };
