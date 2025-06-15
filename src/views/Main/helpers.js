2eb0efeb200349e3b351a07069e0d1bbf272dc79
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-16 02:58:48 +0300

    Format JSON.stringify call in createSessionMetadataFile for readability

--- src/views/Main/helpers.js
+++ src/views/Main/helpers.js
@@ -1,6 +1,8 @@
 export const createSessionMetadataFile = (groupId) => {
     const jsonBlob = new Blob(
-        [JSON.stringify({ groupId: groupId }, null, 2)],
+        [JSON.stringify({
+            groupId: groupId
+        }, null, 2)],
         { type: 'application/json' }
     );
     const file = new File([jsonBlob], `trigger.json`, {
